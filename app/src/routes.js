288b53b252a7b62cdf892c819d43b2d713e7c2d9
gabriel-roque <gabrielrms.dll@gmail.com>
2020-01-16 16:30:14 -0300

    feat(app): code base app and layout finish

---
+++ app/src/routes.js
@@ -0,0 +1,34 @@
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+
+import Main from "./pages/Main";
+import Profile from "./pages/Profile";
+
+const Routes = createAppContainer(
+  createStackNavigator(
+    {
+      Main: {
+        screen: Main,
+        navigationOptions: {
+          title: "finDEV"
+        }
+      },
+      Profile: {
+        screen: Profile,
+        navigationOptions: {
+          title: "Profile Github"
+        }
+      }
+    },
+    {
+      defaultNavigationOptions: {
+        headerTintColor: "#FFF",
+        headerStyle: {
+          backgroundColor: "#7D40E7"
+        }
+      }
+    }
+  )
+);
+
+export default Routes;
