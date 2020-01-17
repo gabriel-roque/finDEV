82339891c4748d7c0367e1bf9b2deef84177c033
gabriel-roque <gabrielrms.dll@gmail.com>
2020-01-17 11:16:55 -0300

    feat(app): add websocket real-time search devs

--- api/src/controllers/DevController.js
+++ api/src/controllers/DevController.js
@@ -1,6 +1,7 @@
 const axios = require("axios");
 const Dev = require("../models/Dev");
 const parseStringAsArray = require("../utils/parseStringAsArray");
+const { findConnections, sendMessage } = require("../websocket");
 
 module.exports = {
   async index(request, response) {
@@ -35,6 +36,14 @@ module.exports = {
         techs: techsArray,
         location
       });
+
+      // Filter new dev for notification socket
+      const sendSocketMessageTo = findConnections(
+        { latitude, longitude },
+        techsArray
+      );
+
+      sendMessage(sendSocketMessageTo, "new-dev", dev);
     }
 
     return response.json(dev);
@@ -63,4 +72,3 @@ module.exports = {
     return await response.json(dev);
   }
 };
-
